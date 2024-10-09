2e836296fe2152fbb21618aaa42d4c5d00cd7b64
snowykte0426 <snowykte0426@naver.com>
2024-10-09 22:25:10 +0900

    Nomad Coders Momentum 클론코딩 하기-2

--- JavaScript/NomadCoders-momentum/app.js
+++ JavaScript/NomadCoders-momentum/app.js
@@ -1,1 +1,8 @@
-alert("hi");
+const age = prompt("Enter your age:");
+if (age !== null) {
+    alert("You are " + age + " years old.");
+    console.log(age);
+} else {
+    alert("No age was entered.");
+    console.log(age)
+}
