fc2bd8a939a96fd496b9e32218b133808706c1d0
snowykte0426 <snowykte0426@naver.com>
2024-10-10 23:54:53 +0900

    Nomad Coders Momentum 클론코딩 하기-7

--- JavaScript/NomadCoders-momentum/app.js
+++ JavaScript/NomadCoders-momentum/app.js
@@ -1,4 +1,5 @@
 const title = document.querySelector(".hello:first-child h1")
+
 title.onmousedown = (event) => {
     event.preventDefault();
 }
@@ -8,7 +9,8 @@ title.ondragstart = (event) => {
 }
 
 let checker = false
-let count = 0;
+let count = 0
+
 function handleTitleClick() {
     if(checker === false){
         title.innerText = "Title is Click!"
@@ -22,9 +24,22 @@ function handleTitleClick() {
     }
 }
 
+function handleTitleHover() {
+    let currentSize = window.getComputedStyle(title).fontSize;
+    title.style.fontSize = parseFloat(currentSize) + 25 + "px";
+}
+
+function handleTitleMouseLeave() {
+    let currentSize = window.getComputedStyle(title).fontSize;
+    title.style.fontSize = parseFloat(currentSize) - 25 + "px";
+}
+
 function clickCounter(){
     count += 1;
     const countOutput = document.querySelectorAll(".hello h1")[1]
     countOutput.innerText = "Count: " + count.toString()
 }
+
 title.addEventListener("click", handleTitleClick)
+title.addEventListener("mouseover", handleTitleHover)
+title.addEventListener("mouseout", handleTitleMouseLeave)
