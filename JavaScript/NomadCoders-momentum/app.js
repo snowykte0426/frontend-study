19fec0eae28bc0bda81e9170f6f403cecd181adb
snowykte0426 <snowykte0426@naver.com>
2024-10-10 23:36:03 +0900

    Nomad Coders Momentum 클론코딩 하기-6

--- JavaScript/NomadCoders-momentum/app.js
+++ JavaScript/NomadCoders-momentum/app.js
@@ -1,2 +1,30 @@
-const title = document.getElementById("title");
-title.innerText="Got you!";
+const title = document.querySelector(".hello:first-child h1")
+title.onmousedown = (event) => {
+    event.preventDefault();
+}
+
+title.ondragstart = (event) => {
+    event.preventDefault();
+}
+
+let checker = false
+let count = 0;
+function handleTitleClick() {
+    if(checker === false){
+        title.innerText = "Title is Click!"
+        clickCounter()
+        checker = true
+    }
+    else {
+        title.innerText = "Grab me!"
+        clickCounter()
+        checker = false
+    }
+}
+
+function clickCounter(){
+    count += 1;
+    const countOutput = document.querySelectorAll(".hello h1")[1]
+    countOutput.innerText = "Count: " + count.toString()
+}
+title.addEventListener("click", handleTitleClick)
