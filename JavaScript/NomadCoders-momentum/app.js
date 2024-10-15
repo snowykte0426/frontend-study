23a3e8a4be72c5080ebc5c9f935146d74130de02
snowykte0426 <snowykte0426@naver.com>
2024-10-15 23:34:15 +0900

    Nomad Coders Momentum 클론코딩 하기-11

--- JavaScript/NomadCoders-momentum/app.js
+++ JavaScript/NomadCoders-momentum/app.js
@@ -3,12 +3,7 @@ const title = document.querySelector(".hello:first-child h1")
 function handleTitleClick() {
     console.log(title)
     const clickedClass = "active"
-    if (title.classList.contains(clickedClass)) {
-        title.classList.remove(clickedClass)
-    }
-    else {
-        title.classList.add(clickedClass)
-    }
+    title.classList.toggle(clickedClass)
 }
 
 title.addEventListener("click", handleTitleClick)
