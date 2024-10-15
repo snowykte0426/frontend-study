33d159473d3a16ba779bd995eb5ce94c8337a901
snowykte0426 <snowykte0426@naver.com>
2024-10-15 22:39:30 +0900

    Nomad Coders Momentum 클론코딩 하기-9

--- JavaScript/NomadCoders-momentum/app.js
+++ JavaScript/NomadCoders-momentum/app.js
@@ -1,57 +1,13 @@
 const title = document.querySelector(".hello:first-child h1")
 
-title.onmousedown = (event) => {
-    event.preventDefault();
-}
-
-title.ondragstart = (event) => {
-    event.preventDefault();
-}
-
-let checker = false
-let count = 0
-
 function handleTitleClick() {
-    if(checker === false){
-        title.innerText = "Title is Click!"
-        clickCounter()
-        checker = true
+    const currentColor = title.style.color
+    if (currentColor === "blue") {
+        title.style.color = "tomato"
     }
     else {
-        title.innerText = "Grab me!"
-        clickCounter()
-        checker = false
+        title.style.color = "blue"
     }
-}
-
-function handleTitleHover() {
-    let currentSize = window.getComputedStyle(title).fontSize;
-    title.style.fontSize = parseFloat(currentSize) + 25 + "px";
-}
-
-function handleTitleMouseLeave() {
-    let currentSize = window.getComputedStyle(title).fontSize;
-    title.style.fontSize = parseFloat(currentSize) - 25 + "px";
-}
-
-function clickCounter(){
-    count += 1;
-    const countOutput = document.querySelectorAll(".hello h1")[1]
-    countOutput.innerText = "Count: " + count.toString()
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato"
-}
-
-function handleWindowCopy() {
-    alert("First alert!");
-    alert("Second alert!");
-    alert("Third alert!");
 }
 
 title.addEventListener("click", handleTitleClick)
-title.addEventListener("mouseover", handleTitleHover)
-title.addEventListener("mouseout", handleTitleMouseLeave)
-window.addEventListener("resize", handleWindowResize)
-window.addEventListener("copy", handleWindowCopy)
