46f7b0aa53e451e003f64ad3bf6ef32201adc370
snowykte0426 <snowykte0426@naver.com>
2024-10-16 14:25:48 +0900

    Nomad Coders Momentum 클론코딩 하기-13

--- JavaScript/NomadCoders-momentum/app.js
+++ JavaScript/NomadCoders-momentum/app.js
@@ -1,10 +1,28 @@
 const loginForm = document.getElementsByClassName("login-form")[0];
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(savedUsername);
+}
