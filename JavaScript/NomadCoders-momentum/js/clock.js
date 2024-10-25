dd082e4613ced4fa6982e32faa3a36f0fbbb0e61
snowykte0426 <snowykte0426@naver.com>
2024-10-25 09:11:33 +0900

    Nomad Coders Momentum 클론코딩 하기-15

--- JavaScript/NomadCoders-momentum/js/clock.js
+++ JavaScript/NomadCoders-momentum/js/clock.js
@@ -1,3 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
-clock.innerText="lalal"
+function getClock() {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const second = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${second}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
