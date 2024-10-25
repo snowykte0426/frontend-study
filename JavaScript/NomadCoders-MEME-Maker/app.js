d5532a4835862a8b31d77f7f07a3b76f44533354
snowykte0426 <snowykte0426@naver.com>
2024-10-26 00:38:56 +0900

    Nomad Coders Meme Maker 만들기-11

--- JavaScript/NomadCoders-MEME-Maker/app.js
+++ JavaScript/NomadCoders-MEME-Maker/app.js
@@ -1,5 +1,8 @@
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
+const modeBtn = document.getElementById("mode-btn");
+const destoryBtn = document.getElementById("destory-btn");
+const EraseBtn = document.getElementById("eraser-btn");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -7,6 +10,7 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 function onMove(event) {
     if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);
@@ -24,6 +28,13 @@ function cancelPainting(event) {
     isPainting = false;
 }
 
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+canvas.addEventListener("click", onCanvasClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMousedown);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -45,4 +56,28 @@ colorOptions.forEach(element => {
         ctx.fillStyle = selectedColor;
         color.value = selectedColor;
     });
+});
+
+modeBtn.addEventListener("click", () => {
+    if (isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    }
+    else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+});
+
+destoryBtn.addEventListener("click", () => {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+});
+
+EraseBtn.addEventListener("click", () => {
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+    ctx.fillStyle = "white";
+    color.value = "white";
 });
