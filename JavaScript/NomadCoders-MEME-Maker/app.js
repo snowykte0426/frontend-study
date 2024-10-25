0d7043a25503585748a17bef10b8331276046405
snowykte0426 <snowykte0426@naver.com>
2024-10-25 22:40:46 +0900

    Nomad Coders Meme Maker 만들기-3

--- JavaScript/NomadCoders-MEME-Maker/app.js
+++ JavaScript/NomadCoders-MEME-Maker/app.js
@@ -6,7 +6,9 @@ ctx.rect(50, 50, 100, 100);
 ctx.rect(150, 150, 100, 100);
 ctx.rect(250, 250, 100, 100);
 ctx.fill();
+ctx.beginPath();
 ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
 ctx.fillStyle = "red";
 setTimeout(() => {
     ctx.fill();
