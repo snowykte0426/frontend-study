4282dd367604a81728e627868060a8643427d036
snowykte0426 <snowykte0426@naver.com>
2024-10-26 00:01:39 +0900

    Nomad Coders Meme Maker 만들기-8

--- JavaScript/NomadCoders-MEME-Maker/app.js
+++ JavaScript/NomadCoders-MEME-Maker/app.js
@@ -3,32 +3,24 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
-
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    "#18dcff",
-    "#7d5fff",
-];
-
-canvas.addEventListener("mousemove", (event) => {
-    ctx.lineTo(event.offsetX, event.offsetY);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.stroke();
-
-    ctx.beginPath();
+let isPainting = false;
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+    }
     ctx.moveTo(event.offsetX, event.offsetY);
-});
+}
 
-canvas.addEventListener("click", () => {
-    const x = Math.random() * canvas.width;
-    const y = Math.random() * canvas.height;
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-});
+function onMousedown(event) {
+    isPainting = true;
+}
+
+function cancelPainting(event) {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMousedown);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
