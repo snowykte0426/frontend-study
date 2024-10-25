941088f94aab87322ab06fe1de11f76c77e63f6d
snowykte0426 <snowykte0426@naver.com>
2024-10-25 09:26:37 +0900

    Nomad Coders Momentum 클론코딩 하기-17

--- JavaScript/NomadCoders-momentum/js/quotes.js
+++ JavaScript/NomadCoders-momentum/js/quotes.js
@@ -40,6 +40,8 @@ const quotes = [
         author: "Margaret Mead"
     }
 ];
-const quote = document.querySelector("#quote span:first-child")
-const author = document.querySelector("#quote span:last-child")
-math.random()
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
