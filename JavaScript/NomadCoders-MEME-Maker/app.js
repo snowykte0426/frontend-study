9c620110c923bbf3ebada7f12f119ee550fce72a
snowykte0426 <snowykte0426@naver.com>
2024-10-26 00:09:46 +0900

    Nomad Coders Meme Maker 만들기-9

--- JavaScript/NomadCoders-MEME-Maker/app.js
+++ JavaScript/NomadCoders-MEME-Maker/app.js
@@ -1,14 +1,16 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 function onMove(event) {
     if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);
         ctx.stroke();
     }
+    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -24,3 +26,7 @@ canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMousedown);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", (event) => {
+    ctx.lineWidth = event.target.value;
+});
