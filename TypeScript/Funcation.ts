1dd04e5320adccf0b491c34f2713ec31e43a805f
snowykte0426 <snowykte0426@naver.com>
2025-01-06 23:23:04 +0900

    TypeScript 매개변수 콜백

--- TypeScript/Funcation.ts
+++ TypeScript/Funcation.ts
@@ -10,3 +10,18 @@ console.log(sum(10, 20));
 console.log(yetAnotherSum(10, 20));
 console.log(onePlusOne());
 console.log(arrowSum(10, 20));
+console.log("-------------------------------------")
+
+function fetchVideo(
+    url: string,
+    success: () => void,
+    failure: () => void
+): void {
+    url === "https://www.youtube.com/watch?v=1234" ? success() : failure();
+}
+
+fetchVideo(
+    "https://www.youtube.com/watch?v=1234",
+    () => console.log("Success"),
+    () => console.log("Failure")
+);
