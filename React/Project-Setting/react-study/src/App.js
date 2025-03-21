40d0342d55365c8fe6214d5d50a0ded45adf3cef
KIM TAEEUN <snowykte0426@naver.com>
2025-03-21 18:22:56 +0900

    React로 틱택토 만들기

--- React/Project-Setting/react-study/src/App.js
+++ React/Project-Setting/react-study/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SquareButton = styled.button`
+  width: 60px;
+  height: 60px;
+  font-size: 24px;
+  cursor: pointer;
+  margin: 4px;
+`;
+
+function Square({ value }) {
+  return <SquareButton>{value}</SquareButton>;
 }
 
-export default App;
+const BoardRow = styled.div`
+  display: flex;
+`;
+
+const OutLine = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
+`;
+
+export default function Board() {
+  return (
+    <OutLine>
+      <BoardRow>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </BoardRow>
+      <BoardRow>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </BoardRow>
+      <BoardRow>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </BoardRow>
+    </OutLine>
+  );
+}
